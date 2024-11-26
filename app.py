d1036138e8cb6f64e4f599e9ff7fcb116aef8f46
s230356 <gimpo.s230356@ggh.goe.go.kr>
2024-11-26 16:42:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
 st.write(' - 학습 데이터 출처 :https://www.kaggle.com/code/sarahyoun/heart-failure-prediction')
 st.write(' - 훈련    데이터 : 210건')
 st.write(' - 테스트 데이터 : 90건')
-st.write(' - 인공지능 모델 정확도 : ***')
 
 # 3.데이터 시각화
 col1, col2, col3, col4, col5 = st.columns(5)  
