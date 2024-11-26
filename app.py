8d5ecc391766936471e14b28e2c4ae86caf78866
s230356 <gimpo.s230356@ggh.goe.go.kr>
2024-11-26 16:22:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,19 +18,19 @@ st.write(' - 인공지능 모델 정확도 : ***')
 col1, col2, col3, col4, col5 = st.columns(5)  
 with col1:
       st.subheader('데이터시각화1')
-      st.image('시각화1.png' )   
+      st.image('시각화1.PNG' )   
 with col2:
       st.subheader('데이터시각화2')
-      st.image('시각화2.png' )  
+      st.image('시각화2.PNG' )  
 with col3:
       st.subheader('데이터시각화3')
-      st.image('시각화3.png')
+      st.image('시각화3.PNG')
 with col4:
       st.subheader('데이터시각화4')
-      st.image('시각화4.png')
+      st.image('시각화4.PNG')
 with col5:
       st.subheader('데이터시각화5')
-      st.image('시각화5.png')
+      st.image('시각화5.PNG')
 
 # 4.모델 활용
 st.subheader('모델 활용')
